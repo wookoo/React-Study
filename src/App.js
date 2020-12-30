eb53acc2bdd3eb53dc81250e732059fd811d12ea
wookoo <nanayagoon@daum.net>
2020-12-30 16:30:17 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React from 'react';
+import Proptyeps from 'prop-types';
+import axios from "axios";
+import Movie from "./Movies";
+import "./App.css";
 
-function Food({foodName}){
-  console.log(foodName);
-  return(
-    <div>
-      <h3>{foodName}</h3>
-      <img src={"https://i.ytimg.com/vi/f08PnZA9AyY/hqdefault.jpg"} alt={foodName}/>
-    </div>
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies : [],
+  }
 
-  );
-}
+  getMovies = async() =>{
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies,isLoading:false});
+    //console.log(movies)
+  }
 
-const FoodList = ["감w자","김치","햄버거","피자","고기","엄"];
-
-
-function App() {
-  return (
-
-    <div>
-      <h1>hellasdo</h1>
-      {FoodList.map(foodName=><Food key={foodName} foodName={foodName}/>)} {/*3번라인의 name 이랑 같음*/}
-    </div>
-
-  );
+  componentDidMount(){
+    this.getMovies();
+  }
+ 
+  render(){
+    const {isLoading,movies} = this.state;
+    return (<section className="contioner">
+       {isLoading ? (
+         <div className="loader">
+           <span className="loader__text">로딩중...</span>
+         </div>
+       ): (
+          <div className = "movies">
+            {movies.map(movies=>(
+              <Movie
+              key = {movies.id}
+              id = {movies.id}
+              year={movies.year}
+              title={movies.title}
+              summary={movies.summary}
+              poster={movies.medium_cover_image}
+              genres = {movies.genres}
+            />)
+            )}
+            
+          </div>
+       )}
+       </section>
+    );
+  }
 }
 
 export default App;
